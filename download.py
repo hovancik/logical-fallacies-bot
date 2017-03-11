41e49998dbeb56f95548e6f77e35ad5d6d7e28eb
Jan Hovancik <conta.srdr@gmail.com>
2017-03-11 09:14:20 +0000

    Adds docstring and use with for file manipulation

--- download.py
+++ download.py
@@ -1,4 +1,7 @@
-# Download fallacies.json from Fallacy.in website
+"""Download fallacies data from Fallacy.in website.
+Location of file on the web: https://fallacy.in/data/fallacies.json.
+Name on the disk after saving: fallacies.json
+"""
 import json
 import urllib.request
 from urllib.error import HTTPError
@@ -6,9 +9,8 @@ from urllib.error import HTTPError
 try:
   req = urllib.request.Request('https://fallacy.in/data/fallacies.json')
   req.add_header('User-Agent', 'Mozilla/5.0')
-  with urllib.request.urlopen(req) as url:
-    f = open('fallacies.json', 'w')
-    data = url.read().decode()    
+  with urllib.request.urlopen(req) as url, open('fallacies.json', 'w') as f:
+    data = url.read().decode()
     f.write(data)
 except HTTPError as ex:
     print(ex.read())
