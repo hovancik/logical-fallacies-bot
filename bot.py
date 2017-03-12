19b0c28d0f3731d0ccded051b445374a0e7a0167
Jan Hovancik <conta.srdr@gmail.com>
2017-03-12 18:14:48 +0000

    adds main() and minor enhancements

--- bot.py
+++ bot.py
@@ -2,9 +2,7 @@ import praw
 import json
 from tinydb import TinyDB, Query
 
-db = TinyDB('./data/db.json')
-
-reddit = praw.Reddit('fallacybot', user_agent='fallacy.in bot by /u/hovancik')
+DB_FILE = './data/db.json'
 
 def reply_text_for(fallacy):
   """Construct the reply of bot for specific fallacy.""" 
@@ -15,28 +13,34 @@ def reply_text_for(fallacy):
   
 def has_reply_on(comment):
   """Check whether the bot has already replied to the Reddit comment"""
-  Comment = Query()
-  return db.contains(Comment.id == comment.id)
+  with TinyDB(DB_FILE) as db:
+    Comment = Query()
+    return db.contains(Comment.id == comment.id)
 
 def set_as_replied_to(comment):
   """Save the Reddit comment's id to the DB to mark it as replied to""" 
-  db.insert({'id': comment.id})
+  with TinyDB(DB_FILE) as db:
+    db.insert({'id': comment.id})
 
-with open('fallacies.json', 'r') as f:
-  fallacies = json.loads(f.read())
-
-subreddit = reddit.subreddit('fallacybottest')
-starters = ["what is ", "what's ", "explain ", "fallacy in "]
-
-for comment in subreddit.stream.comments():
-  normalized_comment = comment.body.lower()
-  for starter in starters: 
-    for fallacy in fallacies:
-      normalized_fallacy = fallacy['title'].lower()
-      if starter + normalized_fallacy in normalized_comment:
-        if not has_reply_on(comment):
-           print("found comment " + comment.id + ": " + comment.body)
-           comment.reply(reply_text_for(fallacy))
-           set_as_replied_to(comment)
-
+def main():
+  reddit = praw.Reddit('fallacybot', user_agent='fallacy.in bot by /u/hovancik')
+  subreddit = reddit.subreddit('fallacybottest')
   
+  with open('fallacies.json', 'r') as f:
+    fallacies = json.loads(f.read())
+
+  starters = ["what is ", "what's ", "explain ", "fallacy in "]
+
+  for comment in subreddit.stream.comments():
+    normalized_comment = comment.body.lower()
+    for starter in starters: 
+      for fallacy in fallacies:
+        normalized_fallacy = fallacy['title'].lower()
+        if starter + normalized_fallacy in normalized_comment:
+          if not has_reply_on(comment):
+             print("found comment " + comment.id + ": " + comment.body)
+             comment.reply(reply_text_for(fallacy))
+             set_as_replied_to(comment)
+
+if __name__ == '__main__':
+    main()  
